22fa6c8cace569ac9b1f0112d08587d9f5829355
francisco <pacolopezbaena@gmail.com>
2014-10-24 16:51:25 +0200

    merged features
    Conflicts:
    	Departamento.py
    	test_departamento.py

--- Empresa.py
+++ Empresa.py
@@ -4,6 +4,6 @@ class Empresa:
     def __init__(self, nombre_empresa, cif, razon_social):
         self.nombre_empresa=nombre_empresa
         self.cif=cif
-        self.razon_social=razon_social
+        self.razon_social=razon_soc
         self.lista_dep=[]
 
