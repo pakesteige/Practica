48d2385a434bd1183eea7b2cbb5a8c42702da954
francisco <pacolopezbaena@gmail.com>
2014-10-24 16:15:20 +0200

    merged features
    Conflicts:
    	Departamento.py
    	test_departamento.py

--- test_departamento.py
+++ test_departamento.py
@@ -1,19 +1,20 @@
 from unittest import TestCase
 from mockito import *
 from Departamento import *
+from Empleado import *
 
 __author__ = 'francisco'
 
 
 class TestDepartamento(TestCase):
     def test_get_salario_total(self):
-        empleado1=mock()
+        empleado1=mock(Empleado)
         when(empleado1).get_salario().thenReturn(200)
 
-        empleado2=mock()
+        empleado2=mock(Empleado)
         when(empleado2).get_salario().thenReturn(100)
 
-        empleado3=mock()
+        empleado3=mock(Empleado)
         when(empleado3).get_salario().thenReturn(300)
 
         departamento = Departamento('Trianon',1)
@@ -24,13 +25,13 @@ class TestDepartamento(TestCase):
         self.assertEqual(departamento.get_salario_total(),600)
 
     def test_get_salario_total_mensual(self):
-        empleado1=mock()
+        empleado1=mock(Empleado)
         when(empleado1).get_salario_mensual().thenReturn(200)
 
-        empleado2=mock()
+        empleado2=mock(Empleado)
         when(empleado2).get_salario_mensual().thenReturn(100)
 
-        empleado3=mock()
+        empleado3=mock(Empleado)
         when(empleado3).get_salario_mensual().thenReturn(300)
 
         departamento = Departamento('Trianon',2)
